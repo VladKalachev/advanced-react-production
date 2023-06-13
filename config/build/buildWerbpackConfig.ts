c55607460803a28c35b1a8bfb3a4f4a8f0d21126
Corvax <vlvete1990@yandex.ru>
2023-06-13 21:15:48 +0300

    chore: added webpack dev server

--- config/build/buildWerbpackConfig.ts
+++ config/build/buildWerbpackConfig.ts
@@ -1,9 +1,9 @@
 import { BuildOptions } from "./types/config";
 import webpack from 'webpack'
-import path from "path";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
   const { mode, paths } = options;
@@ -21,5 +21,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: "inline-source-map",
+    devServer: buildDevServer(options)
   }
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -6,7 +6,7 @@ import { buildWebpackConfig } from './config/build/buildWerbpackConfig';
 import { BuildPaths } from './config/build/types/config';
 
 const paths: BuildPaths = {
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
   build: path.resolve(__dirname, 'build'),
   html: path.resolve(__dirname, 'public', 'index.html'),
   src: path.resolve(__dirname, 'src'),
@@ -16,11 +16,13 @@ const paths: BuildPaths = {
 
 const mode = 'development';
 const isDev = mode === 'development'; 
+const PORT = 4000;
 
 const config: webpack.Configuration = buildWebpackConfig({
   mode: 'development',
   paths,
   isDev,
+  port: PORT,
 })
 
 export default config;
