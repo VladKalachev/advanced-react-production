fa1a0ff3166c29f85f11128b456036af18392afd
Corvax <vlvete1990@yandex.ru>
2023-06-14 23:13:01 +0300

    chore: added webpack sass extract

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 
@@ -9,5 +10,9 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    })
   ]
 }
