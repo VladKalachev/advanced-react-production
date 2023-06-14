a08cb83aadd4c6a257613f580d6f4e02ebce1d64
Corvax <vlvete1990@yandex.ru>
2023-06-14 22:47:14 +0300

    chore: added env

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,26 +3,30 @@ import path from 'path';
 import webpack from 'webpack';
 
 import { buildWebpackConfig } from './config/build/buildWerbpackConfig';
-import { BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  build: path.resolve(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html'),
-  src: path.resolve(__dirname, 'src'),
-  locales: path.resolve(__dirname, 'public', 'locales'),
-  buildLocales: path.resolve(__dirname, 'build', 'locales'),
+export default (env: BuildEnv) => {
+
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
+    src: path.resolve(__dirname, 'src'),
+    locales: path.resolve(__dirname, 'public', 'locales'),
+    buildLocales: path.resolve(__dirname, 'build', 'locales'),
+  };
+  
+  const mode = env.mode || 'development';
+  const PORT = env.port || 4000;
+
+  const isDev = mode === 'development'; 
+  
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  })
+  
+  return config
 };
-
-const mode = 'development';
-const isDev = mode === 'development'; 
-const PORT = 4000;
-
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: 'development',
-  paths,
-  isDev,
-  port: PORT,
-})
-
-export default config;
