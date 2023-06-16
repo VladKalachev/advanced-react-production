31df11340cdf313405c8d17f6c11d97d9ff3e7fa
Corvax <vlvete1990@yandex.ru>
2023-06-16 20:54:10 +0300

    chore: added tuse theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Suspense } from 'react';
+import { Suspense, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
-import "./index.scss";
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
+import "./styles/index.scss";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  
   return (
-    <div>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
