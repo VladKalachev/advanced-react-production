b4fd3854eaa0e164c8c2ddc8415dc8f86f2ccd94
Corvax <vlvete1990@yandex.ru>
2023-06-17 00:42:01 +0300

    chore: added config routers

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,23 @@
+import { Suspense } from 'react'
+import { AboutPage } from '@/pages/AboutPage';
+import { MainPage } from '@/pages/MainPage';
+import { Route, Routes } from 'react-router-dom';
+import { routerConfig } from '@/shared/config/routeConfig/routeConfig';
+
+const AppRouter = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      {Object.values(routerConfig).map(({ path, element }) => (
+        <Route 
+          key={path}
+          path={path}
+          element={element}
+          />
+      ))}
+    </Routes>
+  </Suspense>
+  )
+}
+
+export default AppRouter;
