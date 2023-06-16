605ba59d118229e4f58d75ec4d13e57025af28a1
Corvax <vlvete1990@yandex.ru>
2023-06-16 20:14:02 +0300

    chore: added async components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
-import AboutPage from './pages/AboutPage/AboutPage';
-import MainPage from './pages/MainPage/MainPage';
 
 import "./index.scss";
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
   return (
     <div>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Routes>
-        <Route path='/about' element={<AboutPage/>}/>
-        <Route path='/' element={<MainPage />}/>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/about' element={<AboutPageAsync/>}/>
+          <Route path='/' element={<MainPageAsync />}/>
+        </Routes>
+      </Suspense>
     </div>
   )
 }
