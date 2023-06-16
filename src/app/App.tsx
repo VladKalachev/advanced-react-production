44343cc7437a5851059668b99c25ea5077dcf5ee
Corvax <vlvete1990@yandex.ru>
2023-06-16 23:05:08 +0300

    chore: added feature slice architecture

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,26 @@
-import { Suspense, useContext } from 'react';
+import { Suspense } from 'react';
 import { Link } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom';
 
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { useTheme } from './theme/useTheme';
+import { classNames } from '@/helpers/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AboutPage } from '@/pages/AboutPage';
+import { MainPage } from '@/pages/MainPage';
+
 import "./styles/index.scss";
-import { classNames } from './helpers/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   
   return (
-    <div className={classNames('app', {  }, [theme])}>
+    <div className={classNames('app', { }, [theme])}>
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/about' element={<AboutPageAsync/>}/>
-          <Route path='/' element={<MainPageAsync />}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/' element={<MainPage />}/>
         </Routes>
       </Suspense>
     </div>
