2f3a4d3fc32fe28d4b706e4702cc8f49dec909e8
Corvax <vlvete1990@yandex.ru>
2023-06-16 19:58:43 +0300

    chore: added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Counter } from './components/Counter';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
+
 import "./index.scss";
 
 const App = () => {
   return (
     <div>
-      <Counter/>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Routes>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/' element={<MainPage />}/>
+      </Routes>
     </div>
   )
 }
