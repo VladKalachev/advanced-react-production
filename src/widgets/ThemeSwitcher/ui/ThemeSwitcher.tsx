86285254c61646cbe91d020ff4c8cbdb679c088e
Corvax <vlvete1990@yandex.ru>
2023-06-19 22:46:08 +0300

    chore: adde component icon svg

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,22 +1,22 @@
-import { useTheme } from '@/app/providers/ThemeProvider';
+import { Theme, useTheme } from '@/app/providers/ThemeProvider';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './ThemeSwitcher.module.scss';
-import LightIcon from '@/shared/assets/icons/theme-light.svg";
-import DarkIcon from '@/shared/assets/icons/theme-dark.svg";
+import LightIcon from '@/shared/assets/icons/theme-light.svg';
+import DarkIcon from '@/shared/assets/icons/theme-dark.svg';
 
 interface ThemeSwitcherProps {
   className?: string;
 }
 
 export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
-  const { toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
   
   return (
     <button 
       className={classNames(cls.ThemeSwitcher, {}, [className])}
       onClick={toggleTheme} 
     >
-        TOGGLE
+      { theme === Theme.LIGHT ? <LightIcon/> :  <DarkIcon/>}
     </button>
   )
 }
