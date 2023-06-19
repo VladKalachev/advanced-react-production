07d079da24ef96327d45945b4cf9bf6ecd450526
Corvax <vlvete1990@yandex.ru>
2023-06-19 22:31:26 +0300

    chore: added assets

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,6 +1,8 @@
 import { useTheme } from '@/app/providers/ThemeProvider';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './ThemeSwitcher.module.scss';
+import LightIcon from '@/shared/assets/icons/theme-light.svg";
+import DarkIcon from '@/shared/assets/icons/theme-dark.svg";
 
 interface ThemeSwitcherProps {
   className?: string;
