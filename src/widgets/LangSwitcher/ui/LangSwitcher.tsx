e068a7b0fccbe1d8d972cce3b3ffe42f1637f8c3
Corvax <vlvete1990@yandex.ru>
2023-07-11 11:35:22 +0300

    chore: settings linter

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,26 +1,26 @@
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { Button, ThemeButton } from "@/shared/ui/Button/Button";
 import { useTranslation } from "react-i18next";
-import cls from './LangSwitcher.module.scss'
+import cls from "./LangSwitcher.module.scss";
 
 interface LangSwitcherProps {
   className?: string;
 }
 
 export const LangSwitcher = ({ className }: LangSwitcherProps) => {
- const { t, i18n } = useTranslation()
+  const { t, i18n } = useTranslation();
 
   const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'en' ? "ru" : "en");
-  }
+    i18n.changeLanguage(i18n.language === "en" ? "ru" : "en");
+  };
 
   return (
-      <Button
-        className={classNames(cls.Navbar, {}, [className])}
-        theme={ThemeButton.CLEAR} 
-        onClick={toggle}
-      >
-        {t("Язык")}
-      </Button>
-  )
-}
+    <Button
+      className={classNames(cls.Navbar, {}, [className])}
+      theme={ThemeButton.CLEAR}
+      onClick={toggle}
+    >
+      {t("Язык")}
+    </Button>
+  );
+};
