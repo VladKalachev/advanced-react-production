46afca40a8a49ac6e525d385bc6d6f7f20271664
Corvax <vlvete1990@yandex.ru>
2023-07-24 09:15:20 +0300

    chore: added test

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,7 @@
+import { classNames } from "./classNames";
+
 describe("className", () => {
   test("test", () => {
-    expect(true).toBe(true);
+    expect(classNames("someClass")).toBe("someClass");
   });
 });
