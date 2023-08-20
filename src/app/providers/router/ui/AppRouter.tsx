4897dd89eff7daba5364196d631bf922ec9e18e6
Corvax <vlvete1990@yandex.ru>
2023-08-20 22:57:17 +0300

    chore: added Page Loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,26 @@
-import { Suspense } from 'react'
-import { AboutPage } from '@/pages/AboutPage';
-import { MainPage } from '@/pages/MainPage';
-import { Route, Routes } from 'react-router-dom';
-import { routerConfig } from '@/shared/config/routeConfig/routeConfig';
+import { Suspense } from "react";
+import { AboutPage } from "@/pages/AboutPage";
+import { MainPage } from "@/pages/MainPage";
+import { Route, Routes } from "react-router-dom";
+import { routerConfig } from "@/shared/config/routeConfig/routeConfig";
+import { PageLoader } from "@/widgets/PageLoader";
 
 const AppRouter = () => {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
     <Routes>
       {Object.values(routerConfig).map(({ path, element }) => (
-        <Route 
+        <Route
           key={path}
           path={path}
-          element={<div className='page-wrapper'>{element}</div>}
-          />
+          element={
+            <Suspense fallback={<PageLoader />}>
+              <div className="page-wrapper">{element}</div>
+            </Suspense>
+          }
+        />
       ))}
     </Routes>
-  </Suspense>
-  )
-}
+  );
+};
 
 export default AppRouter;
