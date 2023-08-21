82319d3bcd5c86499b4a36b14bcc12849cda19ec
Corvax <vlvete1990@yandex.ru>
2023-08-21 20:43:57 +0300

    chore: BundleAnalyzerPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,22 +1,27 @@
-import path from 'path';
-import HTMLWebpackPlugin from 'html-webpack-plugin';
+import path from "path";
+import HTMLWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import webpack from 'webpack';
-import { BuildOptions } from './types/config';
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new webpack.ProgressPlugin(),
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css',
-      chunkFilename: 'css/[name].[contenthash:8].css',
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
     }),
     new webpack.HotModuleReplacementPlugin(),
-  ]
+    new BundleAnalyzerPlugin(),
+  ];
 }
