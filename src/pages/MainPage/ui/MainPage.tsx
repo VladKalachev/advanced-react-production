e872e61ce4a2255aae3a5c4f4e8a9b7d1f1b35bf
Corvax <vlvete1990@yandex.ru>
2023-08-21 20:37:32 +0300

    chore: added Bug Button

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,15 @@
-import { useTranslation } from "react-i18next"
+import { BugButton } from "@/app/providers/ErrorBoundary";
+import { useTranslation } from "react-i18next";
 
 const MainPage = () => {
   const [t] = useTranslation();
 
   return (
-    <div>{t('Главная страница')}</div>
-  )
-}
+    <div>
+      <BugButton />
+      {t("Главная страница")}
+    </div>
+  );
+};
 
-export default MainPage
+export default MainPage;
