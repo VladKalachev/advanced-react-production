94ff1790fde66011b566fdac5731f1d9ae1793dc
Corvax <vlvete1990@yandex.ru>
2023-08-24 23:37:28 +0300

    chore: added btn stories

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -6,6 +6,21 @@ export enum ThemeButton {
   CLEAR = "clear",
 }
 
+export enum ButtonTheme {
+  CLEAR = "clear",
+  CLEAR_INVERTED = "clearInverted",
+  OUTLINE = "outline",
+  OUTLINE_RED = "outline_red",
+  BACKGROUND = "background",
+  BACKGROUND_INVERTED = "backgroundInverted",
+}
+
+export enum ButtonSize {
+  M = "size_m",
+  L = "size_l",
+  XL = "size_xl",
+}
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: ThemeButton;
