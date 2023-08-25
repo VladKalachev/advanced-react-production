484b1e2f4e40dba19c6b7bebaf3c6bf14056bedb
Corvax <vlvete1990@yandex.ru>
2023-08-25 17:44:34 +0300

    chore: added config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,15 @@
 import webpack from "webpack";
+import { BuildPaths } from "../build/types/config";
+import path from "path";
 
 export default ({ config }: { config: webpack.Configuration }) => {
+  const paths: BuildPaths = {
+    build: "",
+    html: "",
+    entry: "",
+    src: path.resolve(__dirname, "..", "..", "src"),
+  };
+  config.resolve?.modules?.push(paths.src);
+  config.resolve?.extensions?.push(".ts", ".tsx");
   return config;
 };
