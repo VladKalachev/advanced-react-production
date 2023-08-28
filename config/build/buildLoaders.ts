ff4caa775cb48bfa0d60abad7878bb06f7c7a288
Corvax <vlvete1990@yandex.ru>
2023-08-28 19:15:49 +0300

    chore: added buildLoaders

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,31 +1,14 @@
 import webpack from "webpack";
 import { BuildOptions } from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { buildCssLoader } from "./loaders/buildCssLoader";
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   const svgLoader = {
     test: /\.svg$/,
     use: ["@svgr/webpack"],
   };
 
-  const cssLoader = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-      {
-        loader: "css-loader",
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
-            localIdentName: options.isDev
-              ? "[path][name]__[local]--[hash:base64:5]"
-              : "[hash:base64:8]",
-          },
-        },
-      },
-      "sass-loader",
-    ],
-  };
+  const cssLoader = buildCssLoader(isDev);
 
   // Babel Loader
   const babelLoader = {
