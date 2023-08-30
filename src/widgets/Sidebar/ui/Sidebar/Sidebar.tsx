702de694c9d3670cdc236a132209b178bd2f2d52
Corvax <vlvete1990@yandex.ru>
2023-08-31 01:16:17 +0300

    chore: update Sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -5,6 +5,10 @@ import { ThemeSwitcher } from "@/widgets/ThemeSwitcher";
 import { LangSwitcher } from "@/widgets/LangSwitcher";
 import { Button } from "@/shared/ui/Button";
 import { ButtonSize, ButtonTheme } from "@/shared/ui/Button/Button";
+import { AppLink, AppLinkTheme } from "@/shared/ui/AppLink/AppLink";
+import { RoutePath } from "@/shared/config/routeConfig/routeConfig";
+import AboutIcon from "@/shared/assets/icons/about-20-20.svg";
+import MainIcon from "@/shared/assets/icons/main-20-20.svg";
 
 interface SidebarProps {
   className?: string;
@@ -34,9 +38,28 @@ export const Sidebar = ({ className }: SidebarProps) => {
       >
         {collapsed ? ">" : "<"}
       </Button>
+      <div className={cls.items}>
+        <AppLink
+          className={cls.item}
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.main}
+        >
+          <MainIcon className={cls.icon} />
+          <span className={cls.link}>Главная</span>
+        </AppLink>
+
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          to={RoutePath.about}
+          className={cls.item}
+        >
+          <AboutIcon className={cls.icon} />
+          <span className={cls.link}>О сайте</span>
+        </AppLink>
+      </div>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
+        <LangSwitcher short={collapsed} className={cls.lang} />
       </div>
     </div>
   );
