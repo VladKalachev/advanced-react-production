7efdf979e0853a3bdb9cdefc655759c1f1e52fb3
Corvax <vlvete1990@yandex.ru>
2023-09-02 00:40:37 +0300

    chore: add Counter

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,10 +1,13 @@
+import { counterReducer } from "@/entities/Counter";
 import { configureStore, ReducersMapObject } from "@reduxjs/toolkit";
 import { CombinedState, Reducer } from "redux";
 import { StateSchema } from "./StateSchema";
 
 export function createReduxStore(initialState?: StateSchema) {
   const store = configureStore<StateSchema>({
-    reducer: {},
+    reducer: {
+      counter: counterReducer,
+    },
     devTools: __IS_DEV__,
     preloadedState: initialState,
   });
