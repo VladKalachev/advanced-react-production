955412abbca8206ff31e8ccffeff5d4a07cf9e48
Corvax <vlvete1990@yandex.ru>
2023-09-07 22:53:32 +0300

    chore: add component Input

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { classNames } from "@/shared/lib/classNames/classNames";
 import { Button } from "@/shared/ui/Button";
-import { Modal } from "@/shared/ui/Modal";
+import { Input } from "@/shared/ui/Input";
+
 import { useTranslation } from "react-i18next";
 import cls from "./LoginForm.module.scss";
 
@@ -12,8 +13,8 @@ export const LoginForm = ({ className }: LoginFormProps) => {
   const { t } = useTranslation();
   return (
     <div className={classNames(cls.LoginForm, {}, [className])}>
-      <input type="text" className={cls.input} />
-      <input type="text" className={cls.input} />
+      <Input type="text" className={cls.input} />
+      <Input type="text" className={cls.input} />
       <Button className={cls.loginBtn}>{t("Войти")}</Button>
     </div>
   );
