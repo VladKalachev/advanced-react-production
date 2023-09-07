8e486611fa99e1b91c8f07f9e9edd4146a695de6
Corvax <vlvete1990@yandex.ru>
2023-09-07 22:46:04 +0300

    chore: add LoginForm

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,18 +1,20 @@
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { Button } from "@/shared/ui/Button";
 import { Modal } from "@/shared/ui/Modal";
 import { useTranslation } from "react-i18next";
 import cls from "./LoginForm.module.scss";
 
-interface LoginFormProps {
+export interface LoginFormProps {
   className?: string;
 }
 
-export const LoginForm = () => {
+export const LoginForm = ({ className }: LoginFormProps) => {
   const { t } = useTranslation();
   return (
-    <div>
-      <input type="text" />
-      <input type="text" />
-      <input type="text" />
+    <div className={classNames(cls.LoginForm, {}, [className])}>
+      <input type="text" className={cls.input} />
+      <input type="text" className={cls.input} />
+      <Button className={cls.loginBtn}>{t("Войти")}</Button>
     </div>
   );
 };
