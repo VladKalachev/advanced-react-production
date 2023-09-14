e592219ef2901d39349d59ef30d68386630dc590
Corvax <vlvete1990@yandex.ru>
2023-09-14 22:08:15 +0300

    chore: add stories for Login Form

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -19,6 +19,7 @@ export const userSlice = createSlice({
     },
     logout: (state) => {
       state.authData = undefined;
+      localStorage.removeItem(USER_LOCALSTORAGE_KEY);
     },
   },
 });
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -6,6 +6,9 @@ import { useTranslation } from "react-i18next";
 import { Modal } from "@/shared/ui/Modal";
 import { Button, ButtonTheme } from "@/shared/ui/Button";
 import { LoginModal } from "@/features/AuthByUsername";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserAuthData } from "@/entities/User/model/selectors/getUserAuthData/getUserAuthData";
+import { userActions } from "@/entities/User";
 
 interface NavbarProps {
   className?: string;
@@ -15,6 +18,8 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   const { t } = useTranslation();
   const [isAuthModal, setIsAuthModal] = useState(false);
+  const authData = useSelector(getUserAuthData);
+  const dispatch = useDispatch();
 
   const onShowModal = useCallback(() => {
     setIsAuthModal(true);
@@ -23,6 +28,24 @@ export const Navbar = ({ className }: NavbarProps) => {
   const onCloseModal = useCallback(() => {
     setIsAuthModal(false);
   }, []);
+
+  const onLogout = useCallback(() => {
+    dispatch(userActions.logout);
+  }, [dispatch]);
+
+  if (authData) {
+    return (
+      <div className={classNames(cls.Navbar, {}, [className])}>
+        <Button
+          theme={ButtonTheme.CLEAR_INVERTED}
+          className={cls.links}
+          onClick={onLogout}
+        >
+          {t("Выйти")}
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
