7978b677922b795d93d0c79938af6f7e58a2990f
Corvax <vlvete1990@yandex.ru>
2023-09-16 23:42:08 +0300

    chore: add profile page

---
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -0,0 +1,24 @@
+import { useParams } from "react-router-dom";
+import { classNames } from "@/shared/lib/classNames/classNames";
+// import { Page } from "@/widgets/Page";
+// import { VStack } from '@/shared/ui/redesigned/Stack';
+// import { EditableProfileCard } from "@/features/editableProfileCard";
+
+interface ProfilePageProps {
+  className?: string;
+}
+
+const ProfilePage = ({ className }: ProfilePageProps) => {
+  const { id } = useParams<{ id: string }>();
+
+  return (
+    <div data-testid="ProfilePage" className={classNames("", {}, [className])}>
+      ProfilePage
+      {/* <VStack gap="16" max> */}
+      {/* <EditableProfileCard id={id} /> */}
+      {/* </VStack> */}
+    </div>
+  );
+};
+
+export default ProfilePage;
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,13 +1,14 @@
 import { AboutPage } from "@/pages/AboutPage";
 import { MainPage } from "@/pages/MainPage";
 import { NotFoundPage } from "@/pages/NotFoundPage";
+import { ProfilePage } from "@/pages/ProfilePage";
 import { RouteProps } from "react-router-dom";
 
 export enum AppRoutes {
   MAIN = "main",
   // SETTINGS = "settings",
   ABOUT = "about",
-  // PROFILE = "profile",
+  PROFILE = "profile",
   // ARTICLES = "articles",
   // ARTICLE_DETAILS = "article_details",
   // ARTICLE_CREATE = "article_create",
@@ -21,6 +22,7 @@ export enum AppRoutes {
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: "/",
   [AppRoutes.ABOUT]: "/about",
+  [AppRoutes.PROFILE]: "/profile",
   [AppRoutes.NOT_FOUND]: "*",
 };
 
@@ -32,6 +34,10 @@ export const routerConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.ABOUT]: {
     path: RoutePath.about,
     element: <AboutPage />,
+  },
+  [AppRoutes.PROFILE]: {
+    path: RoutePath.profile,
+    element: <ProfilePage />,
   },
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath.not_found,
