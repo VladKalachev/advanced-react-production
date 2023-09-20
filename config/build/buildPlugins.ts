f24fbb8e7af18fb8f2f8b2828fbaf8d1d1dde98a
Corvax <vlvete1990@yandex.ru>
2023-09-20 20:14:17 +0300

    chore: add __API__

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -8,6 +8,8 @@ import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 export function buildPlugins({
   paths,
   isDev,
+  apiUrl,
+  project,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
   const plugins = [
     new webpack.ProgressPlugin(),
@@ -20,6 +22,8 @@ export function buildPlugins({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
+      __API__: JSON.stringify(apiUrl),
+      __PROJECT__: JSON.stringify(project),
     }),
   ];
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,32 +1,45 @@
-import path from 'path';
+import path from "path";
 
-import webpack from 'webpack';
+import webpack from "webpack";
 
-import { buildWebpackConfig } from './config/build/buildWerbpackConfig';
-import { BuildEnv, BuildPaths } from './config/build/types/config';
+import { buildWebpackConfig } from "./config/build/buildWerbpackConfig";
+import { BuildEnv, BuildPaths, BuildMode } from "./config/build/types/config";
+
+function getApiUrl(mode: BuildMode, apiUrl?: string) {
+  if (apiUrl) {
+    return apiUrl;
+  }
+  if (mode === "production") {
+    return "/api";
+  }
+
+  return "http://localhost:8000";
+}
 
 export default (env: BuildEnv) => {
-
   const paths: BuildPaths = {
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    build: path.resolve(__dirname, 'build'),
-    html: path.resolve(__dirname, 'public', 'index.html'),
-    src: path.resolve(__dirname, 'src'),
-    locales: path.resolve(__dirname, 'public', 'locales'),
-    buildLocales: path.resolve(__dirname, 'build', 'locales'),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    src: path.resolve(__dirname, "src"),
+    locales: path.resolve(__dirname, "public", "locales"),
+    buildLocales: path.resolve(__dirname, "build", "locales"),
   };
-  
-  const mode = env.mode || 'development';
+
+  const mode = env.mode || "development";
   const PORT = env.port || 4000;
 
-  const isDev = mode === 'development'; 
-  
+  const isDev = mode === "development";
+  const apiUrl = getApiUrl(mode, env?.apiUrl);
+
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
     paths,
     isDev,
     port: PORT,
-  })
-  
-  return config
+    apiUrl,
+    project: "frontend",
+  });
+
+  return config;
 };
