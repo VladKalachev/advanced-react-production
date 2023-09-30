9dc8b62e81a2553eb54d49075a41f3ef9459acdd
Corvax <vlvete1990@yandex.ru>
2023-09-30 21:53:22 +0300

    chore: add autonly

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,14 +1,28 @@
-import { Suspense } from "react";
+import { memo, Suspense, useMemo } from "react";
 import { AboutPage } from "@/pages/AboutPage";
 import { MainPage } from "@/pages/MainPage";
 import { Route, Routes } from "react-router-dom";
 import { routerConfig } from "@/shared/config/routeConfig/routeConfig";
 import { PageLoader } from "@/widgets/PageLoader";
+import { useSelector } from "react-redux";
+import { getUserAuthData } from "@/entities/User";
 
 const AppRouter = () => {
+  const isAuth = useSelector(getUserAuthData);
+
+  const routers = useMemo(() => {
+    return Object.values(routerConfig).filter((route) => {
+      if (route.authOnly && !isAuth) {
+        return false;
+      }
+
+      return true;
+    });
+  }, [isAuth]);
+
   return (
     <Routes>
-      {Object.values(routerConfig).map(({ path, element }) => (
+      {routers.map(({ path, element }) => (
         <Route
           key={path}
           path={path}
@@ -23,4 +37,4 @@ const AppRouter = () => {
   );
 };
 
-export default AppRouter;
+export default memo(AppRouter);
