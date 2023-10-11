174ee3e27bd0e5634ab2194de848a2b10351bca3
Corvax <vlvete1990@yandex.ru>
2023-10-11 18:13:20 +0300

    chore: add services article details page

--- src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
+++ src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { CommentCard } from './CommentCard';
-import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+import { CommentCard } from "./CommentCard";
+import { NewDesignDecorator } from "@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator";
 
 export default {
-    title: 'entities/Comment/CommentCard',
-    component: CommentCard,
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
+  title: "entities/Comment/CommentCard",
+  component: CommentCard,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
 } as ComponentMeta<typeof CommentCard>;
 
 const Template: ComponentStory<typeof CommentCard> = (args) => (
-    <CommentCard {...args} />
+  <CommentCard {...args} />
 );
 const normalArgs = {
-    comment: {
-        id: '1',
-        text: 'hello world',
-        user: { id: '1', username: 'Vasya' },
-    },
+  comment: {
+    id: "1",
+    text: "hello world",
+    user: { id: "1", username: "Vasya" },
+  },
 };
 
 export const Normal = Template.bind({});
@@ -32,10 +32,10 @@ NormalRedesigned.decorators = [NewDesignDecorator];
 
 export const Loading = Template.bind({});
 Loading.args = {
-    comment: {
-        id: '1',
-        text: 'hello world',
-        user: { id: '1', username: 'Vasya' },
-    },
-    isLoading: true,
+  comment: {
+    id: "1",
+    text: "hello world",
+    user: { id: "1", username: "Vasya" },
+  },
+  isLoading: true,
 };
