abe1329052249626a5b4d54f4e48b0b35ed89590
Corvax <vlvete1990@yandex.ru>
2023-10-13 00:36:46 +0300

    chore: add article details comment

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,13 +1,22 @@
 import { ArticleDetails } from "@/entities/Article";
 import { classNames } from "@/shared/lib/classNames/classNames";
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from "@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader";
 import { memo } from "react";
-import { useTranslation } from "react-i18next";
+import { ArticleDetailsComments } from "../ArticleDetailsComments/ArticleDetailsComments";
 import { useParams } from "react-router-dom";
+import { articleDetailsPageReducer } from "../../model/slices";
 import cls from "./ArticleDetailsPage.module.scss";
 
 interface ArticleDetailsPageProps {
   className?: string;
 }
+
+const reducers: ReducersList = {
+  articleDetailsPage: articleDetailsPageReducer,
+};
 
 const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
   const { id } = useParams<{ id: string }>();
@@ -17,9 +26,12 @@ const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
   }
 
   return (
-    <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-      <ArticleDetails id={id} />
-    </div>
+    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+      <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+        <ArticleDetails id={id} />
+        <ArticleDetailsComments id={id} />
+      </div>
+    </DynamicModuleLoader>
   );
 };
 
