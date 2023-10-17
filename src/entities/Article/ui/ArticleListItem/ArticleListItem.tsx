97fce101674d125b0a994fcf562bf36a9dfccf50
Corvax <vlvete1990@yandex.ru>
2023-10-17 18:57:59 +0300

    chore: add articles list

---
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -0,0 +1,17 @@
+import { HTMLAttributeAnchorTarget, memo } from "react";
+import { ArticleView } from "../../model/consts/articleConsts";
+import { Article } from "../../model/types/article";
+
+import { ArticleListItemDeprecated } from "./ArticleListItemDeprecated/ArticleListItemDeprecated";
+import { ArticleListItemRedesigned } from "./ArticleListItemRedesigned/ArticleListItemRedesigned";
+
+export interface ArticleListItemProps {
+  className?: string;
+  article: Article;
+  view: ArticleView;
+  target?: HTMLAttributeAnchorTarget;
+}
+
+export const ArticleListItem = memo((props: ArticleListItemProps) => {
+  return <ArticleListItemDeprecated {...props} />;
+});
