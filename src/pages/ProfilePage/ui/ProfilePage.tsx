5bde5fe46b712ad65f426142ace077f5a74e6a25
Corvax <vlvete1990@yandex.ru>
2023-11-04 01:05:48 +0300

    chore: add feature editable profile card

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -27,6 +27,7 @@ import { useTranslation } from "react-i18next";
 import { useInitialEffect } from "@/shared/lib/hooks/useInitialEffect/useInitialEffect";
 import { Page } from "@/widgets/Page";
 import { HStack, VStack } from "@/shared/ui/Stack";
+import { EditableProfileCard } from "@/features/editableProfileCard";
 
 interface ProfilePageProps {
   className?: string;
@@ -38,85 +39,6 @@ const reducers: ReducersList = {
 
 const ProfilePage = ({ className }: ProfilePageProps) => {
   const { id } = useParams<{ id: string }>();
-  const { t } = useTranslation("profile");
-
-  const dispatch = useAppDispatch();
-
-  const formData = useSelector(getProfileForm);
-  const isLoading = useSelector(getProfileIsLoading);
-  const error = useSelector(getProfileError);
-  const readonly = useSelector(getProfileReadonly);
-  const validateErrors = useSelector(getProfileValidateErrors);
-
-  const validateErrorTranslates = {
-    [ValidateProfileError.SERVER_ERROR]: t("Серверная ошибка при сохранении"),
-    [ValidateProfileError.INCORRECT_COUNTRY]: t("Некорректный регион"),
-    [ValidateProfileError.NO_DATA]: t("Данные не указаны"),
-    [ValidateProfileError.INCORRECT_USER_DATA]: t("Имя и фамилия обязательны"),
-    [ValidateProfileError.INCORRECT_AGE]: t("Некорректный возраст"),
-  };
-
-  useInitialEffect(() => {
-    if (id) {
-      dispatch(fetchProfileData(id));
-    }
-  });
-
-  const onChangeFirstName = useCallback(
-    (value?: string) => {
-      dispatch(profileActions.updateProfile({ first: value || "" }));
-    },
-    [dispatch]
-  );
-
-  const onChangeLastName = useCallback(
-    (value?: string) => {
-      dispatch(profileActions.updateProfile({ lastname: value || "" }));
-    },
-    [dispatch]
-  );
-
-  const onChangeCity = useCallback(
-    (value?: string) => {
-      dispatch(profileActions.updateProfile({ city: value || "" }));
-    },
-    [dispatch]
-  );
-
-  const onChangeAge = useCallback(
-    (value?: string) => {
-      dispatch(profileActions.updateProfile({ age: Number(value || 0) }));
-    },
-    [dispatch]
-  );
-
-  const onChangeUsername = useCallback(
-    (value?: string) => {
-      dispatch(profileActions.updateProfile({ username: value || "" }));
-    },
-    [dispatch]
-  );
-
-  const onChangeAvatar = useCallback(
-    (value?: string) => {
-      dispatch(profileActions.updateProfile({ avatar: value || "" }));
-    },
-    [dispatch]
-  );
-
-  const onChangeCurrency = useCallback(
-    (currency: Currency) => {
-      dispatch(profileActions.updateProfile({ currency }));
-    },
-    [dispatch]
-  );
-
-  const onChangeCountry = useCallback(
-    (country: Country) => {
-      dispatch(profileActions.updateProfile({ country }));
-    },
-    [dispatch]
-  );
 
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
@@ -124,32 +46,7 @@ const ProfilePage = ({ className }: ProfilePageProps) => {
         data-testid="ProfilePage"
         className={classNames("", {}, [className])}
       >
-        <VStack max gap="16">
-          <ProfilePageHeader />
-          {validateErrors?.length &&
-            validateErrors.map((err) => (
-              <Text
-                key={err}
-                theme={TextTheme.ERROR}
-                text={validateErrorTranslates[err]}
-                data-testid="EditableProfileCard.Error"
-              />
-            ))}
-          <ProfileCard
-            data={formData}
-            isLoading={isLoading}
-            error={error}
-            readonly={readonly}
-            onChangeFirstName={onChangeFirstName}
-            onChangeLastName={onChangeLastName}
-            onChangeCity={onChangeCity}
-            onChangeAge={onChangeAge}
-            onChangeUsername={onChangeUsername}
-            onChangeAvatar={onChangeAvatar}
-            onChangeCurrency={onChangeCurrency}
-            onChangeCountry={onChangeCountry}
-          />
-        </VStack>
+        <EditableProfileCard id={id} />
       </Page>
     </DynamicModuleLoader>
   );
