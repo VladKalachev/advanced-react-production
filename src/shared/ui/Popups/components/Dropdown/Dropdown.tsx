35d4a3552cb506fb06e73dce1361271f0abef615
Corvax <vlvete1990@yandex.ru>
2023-11-03 23:33:22 +0300

    chore: add avatar dropdown

--- src/shared/ui/Popups/components/Dropdown/Dropdown.tsx
+++ src/shared/ui/Popups/components/Dropdown/Dropdown.tsx
@@ -21,10 +21,6 @@ interface DropdownProps {
   trigger: ReactNode;
 }
 
-/**
- * Устарел, используем новые компоненты из папки redesigned
- * @deprecated
- */
 export function Dropdown(props: DropdownProps) {
   const { className, trigger, items, direction = "bottom right" } = props;
 
