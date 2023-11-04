c7bd7d2212cf55c5e193043f521585321b18e938
Corvax <vlvete1990@yandex.ru>
2023-11-04 17:10:18 +0300

    chore: update react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@/app/providers/ThemeProvider";
@@ -7,7 +7,17 @@ import "./shared/config/i18n/i18n";
 import { ErrorBoundary } from "./app/providers/ErrorBoundary";
 import { StoreProvider } from "./app/providers/StoreProvider";
 
-render(
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error(
+    "Контейнер root не найден. НЕ удалось вмонтировать реакт приложение"
+  );
+}
+
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <StoreProvider>
       <ErrorBoundary>
@@ -16,6 +26,5 @@ render(
         </ThemeProvider>
       </ErrorBoundary>
     </StoreProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
