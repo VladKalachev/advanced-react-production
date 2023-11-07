e9a9e2f0af7e0d2fd2c9c9fcc421c42790f81e11
Corvax <vlvete1990@yandex.ru>
2023-11-08 01:45:19 +0300

    refactor: router

--- src/app/providers/router/ui/RequireAuth.tsx
+++ src/app/providers/router/ui/RequireAuth.tsx
@@ -2,11 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useMemo } from 'react';
 import { getUserAuthData, getUserRoles, UserRole } from '@/entities/User';
-import {
-  getRouteForbidden,
-  getRouteMain,
-  RoutePath,
-} from '@/shared/const/router';
+import { getRouteForbidden, getRouteMain } from '@/shared/const/router';
 
 interface RequireAuthProps {
   children: JSX.Element;
@@ -35,7 +31,7 @@ export function RequireAuth({ children, roles }: RequireAuthProps) {
 
   if (!hasRequiredRoles) {
     return (
-      <Navigate to={RoutePath.forbidden} state={{ from: location }} replace />
+      <Navigate to={getRouteForbidden()} state={{ from: location }} replace />
     );
   }
 
--- src/shared/const/router.ts
+++ src/shared/const/router.ts
@@ -1,31 +1,16 @@
-// export enum AppRoutes {
-//     MAIN = 'main',
-//     SETTINGS = 'settings',
-//     ABOUT = 'about',
-//     PROFILE = 'profile',
-//     ARTICLES = 'articles',
-//     ARTICLE_DETAILS = 'article_details',
-//     ARTICLE_CREATE = 'article_create',
-//     ARTICLE_EDIT = 'article_edit',
-//     ADMIN_PANEL = 'admin_panel',
-//     FORBIDDEN = 'forbidden',
-//     // last
-//     NOT_FOUND = 'not_found',
-// }
-
 export enum AppRoutes {
-  MAIN = "main",
-  // SETTINGS = "settings",
-  ABOUT = "about",
-  PROFILE = "profile",
-  ARTICLES = "articles",
-  ARTICLE_DETAILS = "article_details",
-  ARTICLE_CREATE = "article_create",
-  ARTICLE_EDIT = "article_edit",
-  ADMIN_PANEL = "admin_panel",
-  FORBIDDEN = "forbidden",
-  // last
-  NOT_FOUND = "not_found",
+    MAIN = 'main',
+    SETTINGS = 'settings',
+    ABOUT = 'about',
+    PROFILE = 'profile',
+    ARTICLES = 'articles',
+    ARTICLE_DETAILS = 'article_details',
+    ARTICLE_CREATE = 'article_create',
+    ARTICLE_EDIT = 'article_edit',
+    ADMIN_PANEL = 'admin_panel',
+    FORBIDDEN = 'forbidden',
+    // last
+    NOT_FOUND = 'not_found',
 }
 
 export const getRouteMain = () => "/";
@@ -39,18 +24,7 @@ export const getRouteArticleEdit = (id: string) => `/articles/${id}/edit`;
 export const getRouteAdmin = () => "/admin";
 export const getRouteForbidden = () => "/forbidden";
 
-export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN]: getRouteMain(),
-  [AppRoutes.ABOUT]: getRouteAbout(),
-  [AppRoutes.PROFILE]: getRouteProfile(':id'), // :id
-  [AppRoutes.ARTICLES]: getRouteArticles(),
-  [AppRoutes.ARTICLE_DETAILS]: getRouteArticleDetails(":id"), // :id
-  [AppRoutes.ARTICLE_CREATE]: getRouteArticleCreate(),
-  [AppRoutes.ARTICLE_EDIT]: getRouteArticleEdit(":id"),
-  [AppRoutes.ADMIN_PANEL]: getRouteAdmin(),
-  [AppRoutes.FORBIDDEN]: getRouteForbidden(),
-  [AppRoutes.NOT_FOUND]: "*",
-};
+
 
 // export const AppRouteByPathPattern: Record<string, AppRoutes> = {
 //   [getRouteMain()]: AppRoutes.MAIN,
